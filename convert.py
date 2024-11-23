498b34d4427d64875284edd341336f6e46717bef
Vadim Pushtaev <pushtaev.vm@gmail.com>
2024-11-24 01:39:32 +0200

    New words from Lexin

--- convert.py
+++ convert.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 import sys
 from typing import Generator
 
@@ -14,9 +15,33 @@ def _unimorph_formatter() -> Generator[str, None, None]:
             yield word
 
 
+def _lexin_formatter() -> Generator[str, None, None]:
+    data = json.loads(sys.stdin.read())
+
+    for word_info in data["words"]:
+        try:
+            if word_info.get("pos") != "subst.":
+                continue
+
+            word = word_info["form"].replace("~", "")
+
+            if " " in word or "-" in word:
+                continue
+
+            if any(c.isupper() for c in word):
+                continue
+
+            yield word
+        except:
+            print(f"Error processing word: {word_info}")
+            raise
+
+
 def _get_formatter(from_format: str) -> Generator[str, None, None]:
     if from_format == "unimorph":
         return _unimorph_formatter()
+    elif from_format == "lexin":
+        return _lexin_formatter()
     else:
         raise ValueError(f"Unknown format: {from_format}")
 
